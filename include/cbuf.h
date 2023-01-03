3be5a019d333fb9e33850bffa4e1f72294409d38
Furkan Kurt <kurtfu@yahoo.com>
2023-01-03 21:09:29 +0300

    refactor: make error values consecutive

--- include/cbuf.h
+++ include/cbuf.h
@@ -17,7 +17,7 @@ extern "C" {
 
 #define CBUF_OK               (0)
 #define CBUF_NULL_PTR         (-1)
-#define CBUF_INVALID_CAPACITY (-3)
+#define CBUF_INVALID_CAPACITY (-2)
 
 #define CBUF_DECLARE(T)                               \
     struct cbuf_##T                                   \
